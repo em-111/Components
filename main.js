ef904f7e7d3023e8b50a2b472193d2325700f2a1
em-111 <endritmehaj33@gmail.com>
2024-03-11 22:07:13 +0100

    Components

--- main.js
+++ main.js
@@ -2,15 +2,26 @@
 const aside = document.querySelector("aside");
 const asideContainer = document.querySelector(".aside-container");
 const sidebarBtn = document.getElementById("sidebar-btn");
+const comp = document.querySelectorAll(".comp a");
 
 sidebarBtn.addEventListener("click", () => {
-    if (aside.style.width === "" || aside.style.width === "250px") {
+    if (aside.style.width === "" || aside.style.width === "30px") {
+        aside.style.width = "200px";
+        sidebarBtn.style.rotate = "-180deg";
+        asideContainer.style.display = "block";
+    } else {
         aside.style.width = "30px";
         asideContainer.style.display = "none";
-        sidebarBtn.style.rotate = "180deg";
-    } else {
-        aside.style.width = "250px";
         sidebarBtn.style.rotate = "0deg";
-        asideContainer.style.display = "block";
     }
+});
+
+comp.forEach((c) => {
+    c.addEventListener("click", () => {
+        if (window.innerWidth < 1000) {
+            aside.style.width = "30px";
+            asideContainer.style.display = "none";
+            sidebarBtn.style.rotate = "0deg";
+        }
+    });
 });
